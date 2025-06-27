5deb83a840efb1e5180c07130a8411af386fe65b
Aayush Vyas <aayush25vyas@gmail.com>
2025-06-27 16:53:34 +0530

    Added Food in Snake board

--- script.js
+++ script.js
@@ -8,9 +8,44 @@ document.addEventListener("DOMContentLoaded", () => {
     let food = {x:300 , y:200};
     let snake = [{x:160, y:200}, {x:140, y:200}, {x:120, y:200}];
     let dx = cellSize;  //* displacement with x-axis  
-    let dy = 0;    //* displacement with y-axis 
+    let dy = 0;    //* displacement with y-axis  
 
-    function startGame() { 
+
+    function drawScoreboard() {
+        const scoreBoard = document.getElementById("score-board"); 
+        scoreBoard.textContent = `Score : ${score}`;
+    } 
+
+   function drawDiv(x,y, className) {
+    const div = document.createElement("div"); 
+    div.classList.add(className); 
+    div.style.top = `${y}px`;
+    div.style.left = `${x}px`;  
+
+    return div; 
+   }
+
+    function drawFoodAndSnake() {
+        snakeBoard.innerHTML = ""; 
+        const foodElement = drawDiv(food.x, food.y, 'food'); 
+        snakeBoard.appendChild(foodElement); 
+
+    }
+
+   function gameLoop() {
+    setInterval(() => { 
+        drawScoreboard(); 
+        drawFoodAndSnake(); 
+    }, 1000)
+   } 
+
+   function runGame() {
+    gameStarted = true; 
+    gameLoop(); 
+   }
+
+
+    function initiateGame() { 
         /**
          * Adding score Board 
          */
@@ -27,13 +62,17 @@ document.addEventListener("DOMContentLoaded", () => {
         startButton.classList.add("start-game");
         document.body.appendChild(startButton);
         /** 
-         * 
+         *  Adding eventlistener to startButton 
          */
+        startButton.addEventListener("click", () => {
+            startButton.style.display = "none";
+            runGame();
+        })
 
 
     }
 
-    startGame(); 
+    initiateGame(); 
 
 
 
